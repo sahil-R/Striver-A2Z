90b9635cb3a90cf90d4259988c1de5b978fdaa7b
sahil.ra <sahil.ra@media.net>
2023-09-01 12:23:17 +0530

    p8,p9.py

---
+++ Learn_the_basics/patterns/p9.py
@@ -0,0 +1,18 @@
+j=1
+max=5
+for i in range(max):
+    for temp in range(2*max-j,0,-1):
+        print(" ",end="")
+    for temp in range(j):
+        print("* ",end="")
+    j+=2
+    print("\n")
+j=1
+max=5
+for i in range(max):
+    for temp in range(j):
+        print(" ",end="")
+    for temp in range(0,2*max-j,1):
+        print("* ",end="")
+    j+=2
+    print("\n")
