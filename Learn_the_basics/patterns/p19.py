a0c4725fc1a21d3ea990742758ebceaecfdc8e8c
sahil.ra <sahil.ra@media.net>
2023-09-01 16:29:45 +0530

    p19.py,p21.py

---
+++ Learn_the_basics/patterns/p19.py
@@ -0,0 +1,21 @@
+max=4
+for i in range(max):
+    for j in range(max-i):
+        print("*",end="")
+    for j in range(i):
+        print(" ",end="")
+    for j in range(i):
+        print(" ",end="")
+    for j in range(max-i,0,-1):
+        print("*",end="")
+    print("\n")
+for i in range(max):
+    for j in range(i+1):
+        print("*",end="")
+    for j in range(max-i-1):
+        print(" ",end="")
+    for j in range(max-i-1):
+        print(" ",end="")
+    for j in range(i,-1,-1):
+        print("*",end="")
+    print("\n")
