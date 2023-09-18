fcb622942fb0ad2ec445889cf90665846bf63e5f
sahil.ra <sahil.ra@media.net>
2023-09-18 11:33:48 +0530

    move zero to end best

--- Arrays-1/easy/MoveZero.py
+++ Arrays-1/easy/MoveZero.py
@@ -15,15 +15,31 @@ class Solution:
         #                 break
         # print(nums)
 
+
         
-        
-        for i in range(len(nums)):
-            if nums[i]!=0:
-                temp.append(nums[i])
+        # for i in range(len(nums)):
+        #     if nums[i]!=0:
+        #         temp.append(nums[i])
+        # # print(temp)
+        # for j in range(len(nums)-len(temp)):
+        #     temp.append(0)
         # print(temp)
-        for j in range(len(nums)-len(temp)):
-            temp.append(0)
-        print(temp)
-        for i in range(len(nums)):
-            nums[i]=temp[i]
+        # for i in range(len(nums)):
+        #     nums[i]=temp[i]
         
+        #two pointer modification of the first approach is the optimal solution for this problem
+        n=len(nums)
+        j=None 
+        for i in range(n):
+            if nums[i]==0:
+                j=i
+                break
+        if j!=None:
+            for i in range(j+1,n):
+                if nums[i]!=0:
+                    temp=nums[i]
+                    nums[i]=nums[j]
+                    nums[j]=temp
+                    j+=1
+                
+
