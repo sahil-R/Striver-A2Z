3218aab4d90c5ceb41e8e950cb22749ae8ed7592
sahil.ra <sahil.ra@media.net>
2023-09-23 16:15:21 +0530

    Missing Number

--- Arrays-1/easy/MissingNumber.py
+++ Arrays-1/easy/MissingNumber.py
@@ -4,3 +4,13 @@ def missingNumber(a : List[int], N : int) -> int:
     n=N
     natural_sum=N*(N+1)//2
     return natural_sum-total
+
+#Using XOR
+    def missingNumber(self, nums: List[int]) -> int:
+     xr1=0
+     xr2=0
+     for i in range(len(nums)):
+         xr1^=nums[i]
+         xr2^=(i+1)
+         print(i)
+     return xr1^xr2
